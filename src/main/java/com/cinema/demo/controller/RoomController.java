4f6089ba793e03b6edf9d0871985a535f0137dde
Yassine <yassinemouddene@gmail.com>
2021-08-08 17:16:09 +0100

    Initial commit

---
+++ src/main/java/com/cinema/demo/controller/RoomController.java
@@ -0,0 +1,94 @@
+package com.cinema.demo.controller;
+
+import com.cinema.demo.exception.InvalidPasswordException;
+import com.cinema.demo.exception.InvalidSeatException;
+import com.cinema.demo.model.Room;
+import com.cinema.demo.model.Seat;
+import com.cinema.demo.model.Ticket;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class RoomController {
+
+    private final Map<UUID, Ticket> ticketMap = new HashMap<>();
+    private final String mySuperSecretPassword = "super_secret";
+
+    @GetMapping("/seats")
+    public ResponseEntity<Room> getRoomInfo() {
+        return new ResponseEntity<>(Room.getInstance(), HttpStatus.OK);
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<Ticket> purchaseTicket(@RequestBody Seat seat) {
+        int x = Room.getInstance().getRows();
+        int y = Room.getInstance().getColumns();
+        if (seat.getRowNumber() < 1 || seat.getRowNumber() > x ||
+                seat.getColumnNumber() < 1 || seat.getColumnNumber() > y) {
+            // return new ResponseEntity<>("The number of a row or a column is out of bounds!", HttpStatus.BAD_REQUEST);
+            throw new InvalidSeatException("The number of a row or a column is out of bounds!");
+        }
+
+        if (!Room.getInstance().getAvailableSeats().contains(seat)) {
+            // return new ResponseEntity<>("The Ticket has been already purchased!", HttpStatus.BAD_REQUEST);
+            throw new InvalidSeatException("The Ticket has been already purchased!");
+        }
+
+        Room.getInstance().getAvailableSeats().remove(seat);
+        seat.updatePrice();
+        Ticket ticket = new Ticket(seat);
+        ticketMap.put(ticket.getToken(), ticket);
+        return new ResponseEntity<>(ticket, HttpStatus.OK);
+    }
+
+    @PostMapping("/return")
+    public ResponseEntity<Map<String, Seat>> refundTicket(@RequestBody Map<String, String> json) {
+        if (!json.containsKey("token")) throw new InvalidSeatException("Wrong token!");
+        UUID token = UUID.fromString(json.get("token"));
+        Ticket ticket = ticketMap.getOrDefault(token, null);
+        if (ticket == null) throw new InvalidSeatException("Wrong token!");
+        Room.getInstance().getAvailableSeats().add(ticket.getSeat());
+        ticketMap.remove(token);
+        return new ResponseEntity<>(Map.of("returned_ticket", ticket.getSeat()), HttpStatus.OK);
+    }
+
+    @PostMapping("/stats")
+    public ResponseEntity<Map<String, Integer>> showStatistics(@RequestParam(required = false) String password) {
+        if (!mySuperSecretPassword.equals(password)) throw new InvalidPasswordException();
+        Map<String, Integer> statsMap = new HashMap<>();
+        statsMap.put("current_income", ticketMap.values().stream()
+                .mapToInt(t -> t.getSeat().getPrice())
+                .sum());
+        statsMap.put("number_of_available_seats", Room.getInstance().getAvailableSeats().size());
+        statsMap.put("number_of_purchased_tickets", ticketMap.size());
+
+        return new ResponseEntity<>(statsMap, HttpStatus.OK);
+    }
+
+    /*
+    CUSTOM EXCEPTION HANDLER
+     */
+
+    @ExceptionHandler(InvalidSeatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody
+    Map<String,Object> handleBadRequestException(InvalidSeatException exception) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("error", exception.getMessage());
+        return result;
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public @ResponseBody
+    Map<String,Object> handleUnauthorizedException(InvalidPasswordException exception) {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("error", exception.getMessage());
+        return result;
+    }
+}
---
+++ src/main/java/com/cinema/demo/model/Room.java
@@ -0,0 +1,44 @@
+package com.cinema.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+    private static Room instance;
+
+    private final int rows = 9;
+    private final int columns = 9;
+    private List<Seat> availableSeats;
+
+    public Room() {
+        availableSeats = new ArrayList<>(rows * columns);
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                availableSeats.add(new Seat(i, j));
+            }
+        }
+    }
+
+    @JsonGetter(value = "total_rows")
+    public int getRows() {
+        return rows;
+    }
+
+    @JsonGetter(value = "total_columns")
+    public int getColumns() {
+        return rows;
+    }
+
+    @JsonGetter(value = "available_seats")
+    public List<Seat> getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public static Room getInstance() {
+        if (instance == null) instance = new Room();
+        return instance;
+    }
+}
